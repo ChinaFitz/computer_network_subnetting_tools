3f82223db2b0a7e4d2c851b2999ccf08f5d84d56
chinafitz <498289134@qq.com>
2021-10-20 18:34:30 +0800

    对于utils重新进行分类

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,12 @@
 import bin2dec from "./conversion/bin2dec"
 import dec2bin from "./conversion/dec2bin"
-import address_spliter from "./address_spliter"
-import ipClassifier from "./ipClassifier"
+import address_spliter from "./pretty/spliter"
+import address_joiner from "./pretty/joiner"
+import ipClassifier from "./ip_related/ipClassifier"
 import judgeType from "./judgeType"
-import patchZero from "./patchZero"
-import bin_ip_address from "./conversion/bin_ip_address"
-import address_joiner from "./address_joiner"
-import dec_ip_address from "./conversion/dec_ip_address"
+import patchZero from "./pretty/patchZero"
+import bin_ip_address from "./ip_related/bin_ip_address"
+import dec_ip_address from "./ip_related/dec_ip_address"
 
 export default {
     bin2dec,
