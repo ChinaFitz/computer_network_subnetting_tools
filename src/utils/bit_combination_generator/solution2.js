3e9f43e83ce6ac4da84f1986a9860d0fbe0470cc
chinafitz <498289134@qq.com>
2021-11-10 13:32:15 +0800

    添加了一种更加容易理解的 <列举二进制位的所有组合> 的解决方法, 但是效率比递归解法稍慢

---
+++ src/utils/bit_combination_generator/solution2.js
@@ -0,0 +1,28 @@
+/*
+    @author:  Fitz
+    @name:  bit_combination_generator
+    @description:  二进制位(bin)的组合生成器
+    @parms:  Number 组合的bit位数
+    @return:  Array
+*/
+
+
+// 这种解法更容易理解
+
+export default function(bits) {
+    let result = []
+
+    if (bits === 1) {
+        result.push(['0'], ['1'])
+        return result
+    } else {
+        if (bits > 1) {
+            let r = gen(--bits)
+            for (let i = 0, len = r.length; i < len; i++) {
+                const ele = r[i]
+                result.push(['0', ...ele], ['1', ...ele])
+            }
+        }
+        return result
+    }
+}
